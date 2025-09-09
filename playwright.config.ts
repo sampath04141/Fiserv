4543fd452cb993259e2d4ebf60b44fbb51c05f3a
sampath04141 <sam.auck2@gmail.com>
2025-09-09 22:52:49 +1200

    and junit report

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  reporter: [
     ['html'],
-    ['junit', { outputFile: 'test-results/junit.xml' }] // Add this line
+    ['junit', { outputFile: 'test-results/web-junit.xml' }] // Add this line
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
